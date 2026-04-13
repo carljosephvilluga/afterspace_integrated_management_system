7522f88befd5e04e237edc909dd35ae4108097bb
Althea Shane B. Panes <altheashane.panes@wvsu.edu.ph>
2026-04-13 11:08:31 +0800

    fix: custom text field

--- aims/lib/widgets/common/custom_text_field.dart
+++ aims/lib/widgets/common/custom_text_field.dart
@@ -1,12 +1,13 @@
-//Reusable text field widget
 import 'package:flutter/material.dart';
+import 'package:aims/widgets/utils/validators.dart';
 
-//Reusable text field widget
+//Reusable text field
 class CustomTextField extends StatelessWidget {
   final String hint; //Placeholder
   final double? width; //Optional width for layout flexibility
   final bool isPassword; //if true, hide it
   final TextEditingController? controller; //Read/Write Text
+  final String? Function(String?)? validator;
 
   const CustomTextField({
     super.key,
@@ -14,15 +15,17 @@ class CustomTextField extends StatelessWidget {
     this.width,
     this.isPassword = false, //default: not hidden
     this.controller,
+    this.validator,
   });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: width,
-      child: TextField(
+      child: TextFormField(
         controller: controller,
         obscureText: isPassword,
+        validator: validator,
         decoration: InputDecoration(
           filled: true,
           fillColor: Colors.white,
@@ -30,11 +33,17 @@ class CustomTextField extends StatelessWidget {
           hintStyle: const TextStyle(color: Colors.grey, fontSize: 13),
           contentPadding: const EdgeInsets.symmetric(
             horizontal: 10,
-            vertical: 5,
+            vertical: 12,
           ),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(5),
             borderSide: BorderSide.none,
+          ),
+          //Error styling
+          errorStyle: const TextStyle(fontSize: 11, height: 0.8),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide: const BorderSide(color: Colors.red, width: 0.5),
           ),
         ),
       ),

3082d9682378745194d1950f85630f6e26fe111e
karloantonopenaranda-creator <karloantono.penaranda@wvsu.edu.ph>
2026-04-19 14:34:11 +0800

    fix: flow of pages
    Updated imports and modified the initial route to use RoleSelectionScreen.

--- aims/lib/main.dart
+++ aims/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:aims/screens/dashboard/staff_dashboard_screen.dart';
-import 'package:aims/screens/login/staff_login_screen.dart';
-import 'package:aims/screens/list_of_users/users_list.dart';
+
+import 'screens/dashboard/admin_dashboard_screen.dart';
+import 'screens/dashboard/manager_dashboard_screen.dart';
+import 'screens/dashboard/staff_dashboard_screen.dart';
+import 'screens/login/admin_login_screen.dart';
+import 'screens/login/manager_login_screen.dart';
+import 'screens/login/role_selection_screen.dart';
+import 'screens/login/staff_login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,15 +20,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'AfterSpace',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF80AEC1)),
-        useMaterial3: true,
-      ),
-      initialRoute: '/staff-login',
+      home: const RoleSelectionScreen(),
       routes: {
+        '/admin-login': (context) => const AdminLoginScreen(),
+        '/manager-login': (context) => const ManagerLoginScreen(),
         '/staff-login': (context) => const StaffLoginScreen(),
+        '/admin-dashboard': (context) => const AdminDashboardScreen(),
+        '/manager-dashboard': (context) => const ManagerDashboardScreen(),
         '/staff-dashboard': (context) => const StaffDashboardScreen(),
-        '/staff-users': (context) => const StaffUsersListScreen(),
       },
     );
   }
